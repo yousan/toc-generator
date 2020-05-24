cf3c734f298c30415224e509d45d35a48c212cd5
Yousan_O <yousan@l2tp.org>
2020-05-24 19:41:36 +0900

    Add scripts

--- zeit.go
+++ zeit.go
@@ -3,7 +3,7 @@ package handler
 import (
 	"net/http"
 
-	app "github.com/mini-eggs/go-now-example/app"
+	app "github.com/yousan/toc-generator/app"
 )
 
 func H(w http.ResponseWriter, r *http.Request) {
